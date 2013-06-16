3f296686149c535c3cd6ff1207599052306d168f
suu---- <kila692@gmail.com>
2013-06-16 14:55:20 +0900

    シリアル通信成功
    やったぜ！

--- ADC/firmware/main.c
+++ ADC/firmware/main.c
@@ -5,14 +5,28 @@
 #include"uart0.h"
 #include"uart0.c"
 
+void usart_init(void){
+	UBRR0 = 6;
+
+
+	UCSR0B = _BV(TXEN0) | _BV(RXEN0);
+
+	//frame format
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+}
+void usart_tx(char data){
+	/* Wait for empty transmit buffer */
+	while ((UCSR0A & _BV(UDRE0)) == 0 );
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
+
 void rs_puts(char *st) //文字列を一文字ずつに変換して送信
 {
 	
-	unsigned char ust;
 	
 	while (*st != '\0') {
-		ust = *st;
-		uart0_putc(ust);
+		usart_tx(*st);
 		st++;
 		_delay_ms(10);
 	}
@@ -21,20 +35,16 @@ void rs_puts(char *st) //文字列を一文字ずつに変換して送信
 int main()
 {
 	DDRD|=1<<6;	//PD6を出力に
-	uart0_init();	//uart0初期化
-	sei();	//uart0受信割り込み有効化のためグローバル割り込み有効
+	 usart_init();	//uart初期化
 	int piyo = 100;
 	char foo[64];
-	unsigned char bar;
 	
 	//main loop
 	for(;;)
 	{
 		
-	//	sprintf(foo,"%d\n",piyo);//intを文字列にして代入
-	// 	rs_puts(foo);	//文字列送信
-		bar = 'a';
-		uart0_putc(bar);
+		sprintf(foo,"%d\n\r",piyo);//intを文字列にして代入
+	 	rs_puts(foo);	//文字列送信
 		_delay_ms(100);
 		PORTD^=1<<6;//Toggle LED
 
