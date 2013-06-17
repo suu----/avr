369ef5633033bcb79acee7e74bab142eaa28d5ee
suu---- <kila692@gmail.com>
2013-06-18 00:27:55 +0900

    Merge branch 'master' of https://github.com/suu----/avr
    Conflicts:
    	ADC/firmware/main.c

--- ADC/firmware/main.c
+++ ADC/firmware/main.c
@@ -5,32 +5,57 @@
 #include"uart0.h"
 #include"uart0.c"
 
-void rs_puts(char *st)  //文字列を一文字ずつに変換して送信
+void usart_init(void){
+	UBRR0 = 6;
+
+
+	UCSR0B = _BV(TXEN0) | _BV(RXEN0);
+
+	//frame format
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+}
+void usart_tx(char data){
+	/* Wait for empty transmit buffer */
+	while ((UCSR0A & _BV(UDRE0)) == 0 );
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
+
+void rs_puts(char *st) //文字列を一文字ずつに変換して送信
 {
-    while (*st) {
-        uart0_putc(*st);
-        st++;
-        
-    }
+	
+	
+	while (*st != '\0') {
+		usart_tx(*st);
+		st++;
+		_delay_ms(10);
+	}
 }
 
 int main()
 {
-	DDRD|=1<<6;		//PD6を出力に
-	uart0_init();	//uart0初期化
-	sei();			//uart0受信割り込み有効化のためグローバル割り込み有効
-    int piyo = 100;
-    char foo[64];
-    
+	DDRD|=1<<6;	//PD6を出力に
+	 usart_init();	//uart初期化
+	int piyo = 100;
+	char foo[64];
+	
 	//main loop
 	for(;;)
 	{
+<<<<<<< HEAD
                 
-        sprintf(foo,"%d \r\n",piyo);//intを文字列にして代入
-        rs_puts(foo);	//文字列送信
-        _delay_ms(10);
+        //sprintf(foo,"%d \r\n",piyo);//intを文字列にして代入
+        //rs_puts(foo);	//文字列送信
+        uart0_putc('a');
+        _delay_ms(1000);
+=======
+		
+		sprintf(foo,"%d\n\r",piyo);//intを文字列にして代入
+	 	rs_puts(foo);	//文字列送信
+		_delay_ms(100);
+>>>>>>> 3f296686149c535c3cd6ff1207599052306d168f
 		PORTD^=1<<6;//Toggle LED
-	
-       }
-    return 1;
+
+	}
+	return 1;
 }
